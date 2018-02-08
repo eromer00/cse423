c601dd7467460a4c94776e5c51e0e261920dca55
Franz Chavez <chavezfk@gmail.com>
2018-02-08 11:18:17 -0700

    add printTree inital files

---
+++ printTree.h
@@ -0,0 +1,196 @@
+#ifndef _printtree_H_
+#define _printtree_H_
+
+
+/*
+    Group: Spaghet_Code
+    Members: Erik Romero, Carlos Rubio, Franz Chavez
+    File: printTree.h
+    Description: header file for tree printer
+*/
+
+//Our grammar only permits up to 25 children per node
+#define MAXCHILDREN 100
+
+/*
+* Main kind of Tree Node
+*
+* Declaration node
+* Statement node
+* Expression node
+*/
+typedef enum {DECL, STMT, EXP} NodeKind;
+
+
+/*
+* Kinds of declarations
+*
+* Variable declaration
+* Function declaration
+* Record declaration
+*/
+typedef enum {VAR, FUNC, REC} DeclKind;
+
+/*
+* Kinds of statements
+*
+* If statement
+* While statement
+* Return statement
+* Break statement
+* Compound statement
+*/
+typedef enum {IF, WHILE, RETURN, BREAK, COMP} StmtKind;
+
+/*
+* Kinds of expressions
+*
+* Operation expression
+* Constant expression
+* ID expression
+*/
+typedef enum {OP, CONST, ID} ExpKind;
+
+/*
+* Expression return type
+*
+* Void - Default
+* Number
+* True/False
+* Single character
+*/
+typedef enum {VOID, NUM, TF, SINGLE} ExpType;
+
+/*
+* Operation token
+*
+* plus -     +
+* pplus -   ++
+* dash -     -
+* ddash -   --
+* assign -   =
+* passign - +=
+* sassign - -=
+* massign - *=
+* dassign - /=
+* asterisk - *
+* fslash -   /
+* bNOT -   NOT
+* bAND -   AND
+* bOR -     OR
+* eqeq -    ==
+* neq -     !=
+* lteq -    <=
+* lthan -    <
+* gteq -    >=
+* gthan -    >
+* qmark -    ?
+* mod -      %
+* lsb -      [
+* period -   .
+* colon -    :
+*/
+typedef enum {
+	PLUS, PPLUS, DASH, DDASH, ASSIGN, PASSIGN, SASSIGN, 
+	MASSIGN, DASSIGN, ASTERISK, FSLASH, BNOT, BAND, BOR, EQEQ,
+	NEQ, LTEQ, LTHAN, GTEQ, GTHAN, QMARK, MOD, LSB, PERIOD,
+	COLON } OpKind;
+
+/*
+* Structure to hold AST node properties
+* Essentially chained in a large linked list
+* Holds essential code elements
+*/
+typedef struct TreeNode {
+
+	/*
+	* Pointer to child AST node array
+	*/
+	struct TreeNode* child[MAXCHILDREN];
+
+	/*
+	* Pointer to next AST node
+	*/
+	struct TreeNode* sibling;
+
+	/*
+	* Line number of AST element
+	*/
+	int lineno;
+
+	/*
+	* Node's main type
+	*/
+	NodeKind nodekind;
+
+	/*
+	* Union to hold node's subtype
+	*/
+	union
+	{
+		DeclKind decl;
+		StmtKind stmt;
+		ExpKind exp;
+	} kind;
+
+	/*
+	* Node's main payload
+	*/
+	union
+	{
+		OpKind op;
+		int value;
+		unsigned char cvalue;
+		char* name;
+	} attr;
+
+	/*
+	* Return type of expression
+	*/
+	ExpType expType;
+
+	/*
+	* Holds custom type if rectype var
+	*/
+	char* recType;
+
+	/*
+	* Special flags
+	*
+	* Is an array
+	* Is a record
+	* Is statically declared
+	* Is a function parameter
+	* Is a function call
+	*/
+	int isArray;
+	int isRecord;
+	int isStatic;
+	int isParam;
+	int isFunc;
+
+} TreeNode;
+
+
+/*
+* Print the AST
+*/
+void printTree(FILE* out, TreeNode* tree);
+
+/*
+* Allocate new Declaration node
+*/
+TreeNode* newDeclNode(DeclKind);
+
+/*
+* Allocate new Expression node
+*/
+TreeNode* newExpNode(ExpKind);
+
+/*
+* Allocate new Statement node
+*/
+TreeNode* newStmtNode(StmtKind);
+
+#endif
+
